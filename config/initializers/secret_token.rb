407dcd61428279649c83f5c12012ee46efa2710e
Sonia Feng <sonia.feng@foxmail.com>
2013-10-15 15:59:48 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FristApp::Application.config.secret_key_base = 'ed0fa4554772dca67cc6519fba86d38a8e1bf8c1bf00e6051fed6f7c8e84afb2d12a77a60b1958c0764fa889fc7c3a5f1f9d40b8cf591fcf3ef69d5716b91481'
